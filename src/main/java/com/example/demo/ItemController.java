a4b8426a540b46caae713c18e470557262f8b9c6
Paakhi Iyer <paakhi.iyer@publicissapient.com>
2023-10-07 01:54:22 +0530

    project updated

--- src/main/java/com/example/demo/ItemController.java
+++ src/main/java/com/example/demo/ItemController.java
@@ -1,4 +1,4 @@
-//Author:Paakhi Iyer
+// Author: Paakhi Iyer
 package com.example.demo;
 
 import java.util.List;
@@ -7,14 +7,7 @@ import org.springframework.web.server.ResponseStatusException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/items")
@@ -22,32 +15,37 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
+    // Create a new item
     @PostMapping
     public ResponseEntity<Item> createItem(@RequestBody Item item) {
         Item createdItem = itemService.createItem(item);
-		return ResponseEntity.status(HttpStatus.CREATED).body(createdItem);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdItem);
     }
 
+    // Get a list of all items
     @GetMapping
     public List<Item> getAllItems() {
         return itemService.getAllItems();
     }
 
+    // Get an item by its ID
     @GetMapping("/{id}")
     public ResponseEntity<Item> getItemById(@PathVariable Long id) {
         Item item = itemService.getItemById(id);
-		return ResponseEntity.ok(item);
+        return ResponseEntity.ok(item);
     }
 
+    // Update an existing item
     @PutMapping("/{id}")
     public ResponseEntity<Item> updateItem(@PathVariable Long id, @RequestBody Item updatedItem) {
         Item item = itemService.updateItem(id, updatedItem);
-		return ResponseEntity.ok(item);
+        return ResponseEntity.ok(item);
     }
 
+    // Delete an item by its ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteItem(@PathVariable Long id) {
         itemService.deleteItem(id);
-		return ResponseEntity.noContent().build();
+        return ResponseEntity.noContent().build();
     }
 }
